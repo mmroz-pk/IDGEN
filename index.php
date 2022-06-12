a4cd4480a523ad28d3eb969dab62fd1c324e4227
mmroz-pk <95571899+mmroz-pk@users.noreply.github.com>
2022-06-12 14:34:48 +0200

    Update index.php
    Poprawa stylu kodu

--- index.php
+++ index.php
@@ -3,25 +3,20 @@
 #$tel = file_get_contents("./dane/tel.txt");
 #$email = file_get_contents("./dane/email.txt");
 
-#Imie
+#ImieNazwisko
 $imie= explode("\n", file_get_contents('./dane/imie.txt'));
 $rand_imie = array_rand($imie, 2);
-#echo $imie[$rand_imie[0]] . "\n";
-
-#Nazwisko
-$nazwisko= explode("\n", file_get_contents('./dane/nazwisko.txt'));
-$rand_nazwisko = array_rand($nazwisko, 2);
-#echo $nazwisko[$rand_nazwisko[0]] . "\n";
+$show_imie = $imie[$rand_imie[0]] . "\n";
 
 #Adres
 $adres= explode("\n", file_get_contents('./dane/adres.txt'));
 $rand_adres = array_rand($adres, 2);
-#echo $adres[$rand_adres[0]] . "\n";
+$show_adres = $adres[$rand_adres[0]] . "\n";
 
 #Urodziny
 $urodziny= explode("\n", file_get_contents('./dane/urodziny.txt'));
 $rand_urodziny = array_rand($urodziny, 2);
-#echo $urodziny[$rand_urodziny[0]] . "\n";
+$show_urodziny =  $urodziny[$rand_urodziny[0]] . "\n";
 
 ?>
 <!doctype html>
@@ -40,12 +35,24 @@ $rand_urodziny = array_rand($urodziny, 2);
 </div>
 <div class="data">
     <p><img src="https://www.thispersondoesnotexist.com/image" height="250px"></p>
-    <p>Imię i Naziwsko:</p><p><input type="text" value="<?php echo $imie[$rand_imie[0]] . "\n";?>" disabled></p>
-    <p>Adres zamieszkania:</p><p><textarea rows="5" disabled><?php echo $adres[$rand_adres[0]] . "\n"; ?></textarea></p>
-    <p>Data urodzenia:</p><p><input type="text" value="<?php echo $urodziny[$rand_urodziny[0]] . "\n"; ?>" disabled></p>
+
+    <p>Imię i Naziwsko:</p>
+        <p><input type="text" value="<?php echo $show_imie; ?>" disabled></p>
+    <p>Adres zamieszkania:</p>
+        <p><textarea rows="5" disabled><?php echo $show_adres; ?></textarea></p>
+    <p>Data urodzenia:</p>
+        <p><input type="text" value="<?php echo $show_urodziny; ?>" disabled></p>
+
     <p>---</p>
-    <p>Numer telefonu: </p><p><input type="text" value="" disabled></p><p><button>Sprawdź SMS</button></p>
-    <p>Adres E-Mail: </p><p><input type="text" value="" disabled></p><p><button>Sprawdź Pocztę</button></p>
+
+    <p>Numer telefonu: </p>
+        <p><input type="text" value="+48 727 801 893" disabled></p>
+            <p><a target="_blank" href="https://receive-smss.com/sms/48727801893/"><button>Sprawdź SMS</button></a></p>
+
+    <p>Adres E-Mail: </p>
+        <p><input type="text" value="<?php echo $show_imie . "@einrot.com"; ?>" disabled></p>
+            <p><a target="_blank" href="<?php echo "http://www.fakemailgenerator.com/#/einrot.com/$show_imie";?>"><button>Sprawdź Pocztę</button></a></p>
+
 </div>
 </body>
 </html>
