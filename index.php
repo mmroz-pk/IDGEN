f0fcfe12bb0caa7bcae605b95940cdb60da66621
mmroz-pk <95571899+mmroz-pk@users.noreply.github.com>
2022-06-12 12:42:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>IdGen</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="header">
+    <p>IdGen</p>
+</div>
+<div class="data">
+    <p><img src="https://www.thispersondoesnotexist.com/image" height="250px"></p>
+    <p>Imię:</p><p><input type="text" value="" disabled></p>
+    <p>Nazwisko:</p><p><input type="text" value="" disabled></p>
+    <p>Adres zamieszkania:</p><p><textarea disabled></textarea></p>
+    <p>---</p>
+
+    <p>Nazwisko panieńskie matki: </p><p><input type="text" value="" disabled></p>
+    <p>Pesel: </p><p><input type="text" value="" disabled></p>
+</div>
+</body>
+</html>
