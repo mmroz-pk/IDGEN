47840302b6c6d15c6e2ef5da661304f50cf4a777
mmroz-pk <95571899+mmroz-pk@users.noreply.github.com>
2022-06-30 17:08:34 +0200

    Update connect_imap.php
    update

--- connect_imap.php
+++ connect_imap.php
@@ -1,19 +1,24 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+#ini_set('display_errors', '1');
+#ini_set('display_startup_errors', '1');
+#error_reporting(E_ALL);
 
 include_once __DIR__ . '/vendor/autoload.php';
 
 #Connect to imap server
 
+$imap_host = "";
+$imap_port = "";
+$imap_user = "";
+$imap_pass = "";
+
 use Clivern\Imap\Core\Connection;
 
 $connection = new Connection(
-    "imap_host",
-    "imap_port",
-    "imap_username",
-    "imap_password",
+    "$imap_host",
+    "$imap_port",
+    "$imap_user",
+    "$imap_pass",
     "/ssl/novalidate-cert",
     "INBOX"
 );
