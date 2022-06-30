022f6ae15ad309f512da3b47e369f91818c49e8c
mmroz-pk <95571899+mmroz-pk@users.noreply.github.com>
2022-06-30 17:10:09 +0200

    Create connect.php
    update

---
+++ php/connect.php
@@ -0,0 +1,10 @@
+<?php
+$db_host = "";
+$db_port = "";
+$db_database = "";
+$db_user = "";
+$db_pass = "";
+
+$connect = pg_connect("host=$db_host port=$db_port dbname=$db_database user=$db_user password=$db_pass");
+
+?>
