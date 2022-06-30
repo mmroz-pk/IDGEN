e2027a97a8e6a43f56b469696ba83de2c724b6c4
mmroz-pk <95571899+mmroz-pk@users.noreply.github.com>
2022-06-30 12:57:13 +0200

    Create search_imap.php
    refresh imap fix

---
+++ search_imap.php
@@ -0,0 +1,38 @@
+<?php
+include_once __DIR__ . '/vendor/autoload.php';
+
+$email = $_GET['e'];
+
+#Search specific email
+use Clivern\Imap\Core\Search;
+use Clivern\Imap\Core\Search\Condition\To;
+use Clivern\Imap\MailBox;
+
+use Clivern\Imap\Core\Connection;
+
+
+$connection = new Connection(
+    "imap_host",
+    "imap_port",
+    "imap_username",
+    "imap_password",
+    "/ssl/novalidate-cert",
+    "INBOX"
+);
+$connection->connect();
+
+$search = new Search();
+$search->addCondition(new To("$email"));
+
+$mailbox = new MailBox($connection);
+
+$messages = $mailbox->getMessages($search);
+
+foreach ($messages as $message) {
+    echo "From: " . $message->header()->get('from');
+    echo "| ";
+    echo "Subject: " . $message->header()->get('subject');
+    echo "<br>";
+    echo $message->body()->getMessage();
+}
+?>
