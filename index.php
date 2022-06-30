91e11a2321410951c2b72c1192f6076565e2b04a
mmroz-pk <95571899+mmroz-pk@users.noreply.github.com>
2022-06-30 12:56:09 +0200

    Update index.php
    refresh imap fix

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include("connect_imap.php");
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./assets/css/style.css">
-
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <title>idGen</title>
 </head>
 <body>
@@ -40,30 +40,13 @@ include("connect_imap.php");
         </div>
         <div class="right" id="right">
             <p class="right-title">&nbsp;Here is where you receive emails</p>
-        <p class="email">
-	<?php
-	#Search specific email
-	use Clivern\Imap\Core\Search;
-	use Clivern\Imap\Core\Search\Condition\To;
-	use Clivern\Imap\MailBox;
-
-	
-	$search = new Search();
-	$search->addCondition(new To("$name@shizzap.biz"));
-
-	$mailbox = new MailBox($connection);
-
-	$messages = $mailbox->getMessages($search);
-
-	foreach ($messages as $message) {
-    	echo "From: " . $message->header()->get('from');
-    	echo "| ";
-    	echo "Subject: " . $message->header()->get('subject');
-    	echo "<br>";
-    	echo $message->body()->getMessage();
-	}
-	?>
-	</p>
+            <script>
+                var auto_refresh = setInterval(
+                    (function () {
+                        $("#email").load("search_imap.php?e=<?php echo $email; ?>"); //Load the content into the div
+                    }), 1000);
+            </script>
+        <div class="email" id="email"></div>
         </div>
     </div>
 <div class="footer">
